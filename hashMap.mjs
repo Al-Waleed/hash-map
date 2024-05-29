af389e1116efa893997b4229b5f92c75c4912adf
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-29 20:18:25 +0300

    Create a hash function

--- hashMap.mjs
+++ hashMap.mjs
@@ -1,6 +1,14 @@
 function hashMap() {
-  const map = new Array(16)
+  const map = new Array(16);
 
-  
+  const hash = (key) => {
+    let hashCode = 0;
 
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
+    }
+
+    return hashCode;
+  };
 }
