ee3cf13328f7fd56a212e74343f8701ef40ac04a
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-29 23:21:52 +0300

    Edit the set function

--- hashMap.mjs
+++ hashMap.mjs
@@ -1,3 +1,5 @@
+import node from "./node.mjs";
+
 function hashMap() {
   const map = new Array(16);
 
@@ -13,14 +15,24 @@ function hashMap() {
   };
 
   const set = (key, value) => {
-    const hashed = hash(key);
-    // add the key value pain to the index of the hash if its empty
-    if (map[hashed] === undefined) {
-      map[hashed] = { key: value };
+    const index = hash(key);
+    // add the key and value node to the map using the index ---> (hash(key))
+    if (map[index] === undefined) {
+      // this creates a node with the key, value and next, we need next in case we need to add more nodes to the same index
+      map[index] = node(key, value);
+      // in case the first node has the same key we update its value
+    } else if (map[index].key === key) {
+      map[index] = node(key, value, map[index].next);
+    } else {
+      // to append the new node to the .next of the last node
+      let pointer = map[index];
+      // to reach the last node
+      while (pointer.next !== null) {
+        //!!! i need to create a function that checks if the key exits and update its value
+        pointer = pointer.next;
+      }
+      pointer.next = node(key, value);
     }
-    // else if (map[hashed] !== undefined) {
-    //   // code for linked lists (ill add it later)
-    // }
   };
 
   return {
@@ -29,8 +41,13 @@ function hashMap() {
   };
 }
 
-const testHashMap = hashMap()
+const testHashMap = hashMap();
 
-testHashMap.set("waleed", "age:24");
+testHashMap.set("test", "0");
+testHashMap.set("test1", "1");
+testHashMap.set("test2", "2");
+testHashMap.set("test3", "3");
+testHashMap.set("test4", "4");
+testHashMap.set("test5", "5");
 
-console.log(testHashMap.map);
+console.log(testHashMap.map)
