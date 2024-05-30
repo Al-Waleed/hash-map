6819a76bb4ab4cf2c3fc75f1c17ecf05bcbb2675
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-30 21:24:58 +0300

    Fix a bug in has function now it checks if the bucket is empty or not before checking nested nodes && edit remove function to use has() function in it to check if the key exist or not before removing it

--- hashMap.mjs
+++ hashMap.mjs
@@ -59,65 +59,72 @@ function hashMap() {
   // the same code as get() but instead of a value it returns true or false
   const has = (key) => {
     const index = hash(key);
-    // to check if the key at the specified index = the key we entered
-    if (map[index].key === key) {
-      return true;
-    } else {
-      // check every node inside the linked list until we find the key and return true
-      let pointer = map[index];
-      while (pointer.next !== null) {
-        pointer = pointer.next;
-        if (pointer.key === key) {
-          return true;
+    // we need to check if the bucket is empty or not
+    if (map[index] !== undefined) {
+      // to check if the key at the specified index = the key we entered
+      if (map[index].key === key) {
+        return true;
+      } else {
+        // check every node inside the linked list until we find the key and return true
+        let pointer = map[index];
+        while (pointer.next !== null) {
+          pointer = pointer.next;
+          if (pointer.key === key) {
+            return true;
+          }
         }
       }
-    }
-    // if the key isn't found we return false
-    return false;
+      // if the key isn't found we return false
+      return false;
+      // if the bucket is empty it means the key doesn't exist so we return false
+    } else return false;
   };
 
   const remove = (key) => {
-    const index = hash(key);
-    // check if the key is at the head of the linked list
-    if (map[index].key === key) {
-      // when the node that has the key is the head node we check if there's a node after it
-      // if there's another node after it then the node we want to delete must equal to the next node
-      if (map[index].next !== null) {
-        map[index] = node(
-          // this refers to the key,value and .next of the node after the one we want to delete
-          map[index].next.key,
-          map[index].next.value,
-          map[index].next.next
-        );
-        return true;
-      } else {
-        // if there isn't a node after it then we delete it without worrying about the next node
-        map[index] = undefined;
-        return true;
-      }
-      // to if there's another node in the list
-    } else if (map[index].next !== null) {
-      let pointer = map[index];
-      // to loop over the nodes in the list but using .next.next stops at the node second to last because we want to be able to check its .next.key then remove it if it matches
-      while (pointer.next.next !== null) {
-        // if the key on the next node matches we remove it 
-        if (pointer.next.key === key) {
-          pointer.next = node(
-            pointer.next.next.key,
-            pointer.next.next.value,
-            pointer.next.next.next
+    // check if the key is in the list first using has()
+    if (has(key)) {
+      const index = hash(key);
+      // check if the key is at the head of the linked list
+      if (map[index].key === key) {
+        // when the node that has the key is the head node we check if there's a node after it
+        // if there's another node after it then the node we want to delete must equal to the next node
+        if (map[index].next !== null) {
+          map[index] = node(
+            // this refers to the key,value and .next of the node after the one we want to delete
+            map[index].next.key,
+            map[index].next.value,
+            map[index].next.next
           );
           return true;
+        } else {
+          // if there isn't a node after it then we delete it without worrying about the next node
+          map[index] = undefined;
+          return true;
         }
-        pointer = pointer.next;
+        // to if there's another node in the list
+      } else if (map[index].next !== null) {
+        let pointer = map[index];
+        // to loop over the nodes in the list but using .next.next stops at the node second to last because we want to be able to check its .next.key then remove it if it matches
+        while (pointer.next.next !== null) {
+          // if the key on the next node matches we remove it
+          if (pointer.next.key === key) {
+            pointer.next = node(
+              pointer.next.next.key,
+              pointer.next.next.value,
+              pointer.next.next.next
+            );
+            return true;
+          }
+          pointer = pointer.next;
+        }
+        // we still have to check the last node in the list
+        if (pointer.next.key === key) {
+          pointer.next = null;
+          return true;
+        }
       }
-      // we still have to check the last node in the list 
-      if (pointer.next.key === key) {
-        pointer.next = null;
-        return true;
-      }
-    } 
-      return false;
+      // if the key doesn't exist we return false
+    } else return false;
   };
 
   return {
@@ -152,10 +159,10 @@ console.log(testHashMap.remove("a"));
 console.log(testHashMap.remove("aaa"));
 console.log(testHashMap.remove("test4"));
 console.log(testHashMap.remove("test2"));
-console.log(testHashMap.remove("test3"));
-console.log(testHashMap.remove("test"));
-console.log(testHashMap.remove("test1"));
-console.log(testHashMap.remove("test5"));
+// console.log(testHashMap.remove("test2"));
+// console.log(testHashMap.remove("test3"));
+// console.log(testHashMap.remove("test"));
+// console.log(testHashMap.remove("test1"));
 
 console.log(testHashMap.map);
 
@@ -163,7 +170,7 @@ console.log(testHashMap.map);
 // console.log(testHashMap.has("aa"));
 // console.log(testHashMap.has("aab"));
 // console.log(testHashMap.has("aaa"));
-// console.log(testHashMap.has("test2"));
+console.log(testHashMap.has("test2"));
 // console.log(testHashMap.has("test3"));
 // console.log(testHashMap.has("test4"));
 // console.log(testHashMap.has("test5"));
